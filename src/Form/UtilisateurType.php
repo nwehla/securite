5090463c472415b43fdf6b905c3eeabf1ae313d7
nwehla <nwehlabatoum@laposte.net>
2021-11-18 01:00:07 +0100

    utilisateursType choiceType

--- src/Form/UtilisateurType.php
+++ src/Form/UtilisateurType.php
@@ -4,10 +4,13 @@ namespace App\Form;
 
 use App\Entity\Utilisateurs;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Date;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class UtilisateurType extends AbstractType
@@ -18,12 +21,18 @@ class UtilisateurType extends AbstractType
             ->add('nom',TextType::class)
             ->add('prenom',TextType::class)
             ->add('photo',TextType::class)
-            ->add('datedenaissance')
+            ->add('datedenaissance',DateType::class, [
+                'widget' => 'single_text',
+            ])
             ->add('login',TextType::class)
             ->add('motdepasse',TextType::class)
             ->add('adresse',TextType::class)
-            ->add('email',EmailType::class)
-            ->add('role',TextType::class)
+            ->add('email',EmailType::class,["attr"=>[
+                "placeholder"=>"entrez un email valide"
+            ]])
+            ->add('role',ChoiceType::class , [
+                "choices" =>["proprietaire"=>"proprietaire","locataire"=>"locataire","administrateur"=>"administrateur","gestionnaire"=>"administrateur"]
+            ])
             ->add('envoyer',SubmitType::class)
         ;
     }
