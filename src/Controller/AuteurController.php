0ebe05e60b31fabb1748b7250d2c4e46a6fa446e
nwehla <nwehlabatoum@laposte.net>
2021-11-23 01:47:29 +0100

    il reste de mettre les auteurs de 'article dans affichage

--- src/Controller/AuteurController.php
+++ src/Controller/AuteurController.php
@@ -2,9 +2,18 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
+
+
+use App\Entity\Auteurs;
+use App\Form\AuteursType;
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Repository\AuteursRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class AuteurController extends AbstractController
 {
@@ -15,8 +24,59 @@ class AuteurController extends AbstractController
     {
         $repo=$this->getDoctrine()->getRepository(Auteurs::class);
         $auteurs=$repo->findAll();
-        return $this->render('auteur/auteur.html.twig', [
+        return $this->render('auteurs/auteur.html.twig', [
             'auteurs'=>$auteurs,
         ]);
     }
+    
+    /**
+     * @Route("/{id}/edit",name="edit_auteur")
+     */
+    public function editer(EntityManagerInterface $manager,Request $request,Auteurs $auteur): Response
+    {
+        $form=$this->createForm(AuteursType::class,$auteur);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()){
+                $manager->flush();
+                return $this->redirectToRoute("affichage_auteur")
+
+       ;     }
+
+        return $this->render('auteurs/formulaire.html.twig', [
+            'form'=>$form->createView(),
+        ]);
+    }
+    /**
+     * @Route("/formulaire", name="form_auteur")
+     */
+    public function ajouter(EntityManagerInterface $manager,Request $request): Response
+    {
+        $auteur=new Auteurs();
+        $form=$this->createForm(AuteursType::class);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()){
+                $manager->persist($auteur);
+                $manager->flush();
+                return $this->redirectToRoute("affichage_auteur",["id"=>$auteur->getId()])
+
+       ;     }
+
+        return $this->render('auteurs/formulaire.html.twig', [
+            'form'=>$form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="affi_auteur")
+     */
+    public function afficher(Auteurs $auteur): Response
+    {
+
+        return $this->render('auteurs/affiche.html.twig', [
+            'id'=>$auteur->getId(),
+            'auteur'=>$auteur,
+        ]);
+    }
+    
+
 }
